de93a5d7ee6d8d832ef4315bcabd89c5a6045be6
kiramekiki <okjenny54@gmail.com>
2026-01-08 21:59:43 +0800

    Update get.js

--- api/get.js
+++ api/get.js
@@ -1,10 +1,15 @@
 export default async function handler(req, res) {
-  const response = await fetch(`${process.env.SUPABASE_URL}/rest/v1/collection?select=*`, {
+  const supabaseUrl = process.env.SUPABASE_URL;
+  const supabaseKey = process.env.SUPABASE_KEY;
+
+  // 直接抓取 collection 表格裡的所有資料
+  const response = await fetch(`${supabaseUrl}/rest/v1/collection?select=*`, {
     headers: {
-      'apikey': process.env.SUPABASE_KEY,
-      'Authorization': `Bearer ${process.env.SUPABASE_KEY}`
+      'apikey': supabaseKey,
+      'Authorization': `Bearer ${supabaseKey}`
     }
   });
+
   const data = await response.json();
   res.status(200).json(data);
 }
